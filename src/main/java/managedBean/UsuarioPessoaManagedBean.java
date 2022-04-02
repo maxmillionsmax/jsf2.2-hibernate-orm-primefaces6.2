f445e155678f834e15eec8b6fabcea220a59cf3c
maxmillionsmax <maximilianocaetano@gmail.com>
2022-04-02 00:39:43 -0300

    Deletando cadastros

--- src/main/java/managedBean/UsuarioPessoaManagedBean.java
+++ src/main/java/managedBean/UsuarioPessoaManagedBean.java
@@ -12,8 +12,8 @@ import model.UsuarioPessoa;
 
 @ManagedBean(name = "usuarioPessoaManagedBean")
 @ViewScoped
-public class UsuarioPessoaManagedBean implements Serializable{
-	
+public class UsuarioPessoaManagedBean implements Serializable {
+
 	private static final long serialVersionUID = 1L;
 
 	private UsuarioPessoa usuarioPessoa = new UsuarioPessoa();
@@ -31,20 +31,25 @@ public class UsuarioPessoaManagedBean implements Serializable{
 	}
 
 	public String salvar() {
-		
+
 		daoGeneric.salvar(usuarioPessoa);
 
 		return "";
 	}
-	
-	
+
 	public String novo() {
-		usuarioPessoa =  new UsuarioPessoa();
+		usuarioPessoa = new UsuarioPessoa();
 		return "";
 	}
-	
+
 	public List<UsuarioPessoa> getList() {
 		list = daoGeneric.listar(UsuarioPessoa.class);
 		return list;
+	}
+
+	public String remover() {
+		daoGeneric.deletarPorId(usuarioPessoa);
+		usuarioPessoa = new UsuarioPessoa();
+		return "";
 	}
 }
